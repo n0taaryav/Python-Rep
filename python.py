14293f65a24c69560c8434cc8a670382fd265596
n0taaryav <128344052+n0taaryav@users.noreply.github.com>
2023-03-20 09:51:31 +0530

    Python Rep
    Python Rep

---
+++ python.py
@@ -0,0 +1,58 @@
+x = 1
+y = 5
+
+print(x+y)
+
+print(x-y)
+
+print(x*y)
+
+print(x/y)
+
+print(x%y)
+
+print(x**y)
+
+print(x//y)
+
+print("part 2")
+##------------------------------------------------------##
+
+if x==y:
+    print("sui")
+
+if x<y:
+    print("sui")
+
+if x>y:
+    print("sui")
+
+if x!=y:
+    print("sui")
+
+if x>=y:
+    print("sui")
+
+if x<=y:
+    print("sui")
+
+print("part 3")
+##------------------------------------------------------##
+
+if x and y == 0:
+    print("no sui")
+
+if x or y != 0:
+    print("sui")
+
+print("part 4")
+##------------------------------------------------------##
+
+x += y
+print(x)
+
+x -= y
+print(x)
+
+x *= y
+print(x)
